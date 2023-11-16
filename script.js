4fafac4a1a33ab4d72fb59ba3306313f2637b01d
Olena Shevchenko <lemonslice67@gmail.com>
2023-11-16 10:44:18 +0100

    added amazing button

---
+++ script.js
@@ -0,0 +1,57 @@
+"use strict";
+
+
+// Button animation
+
+window.addEventListener('load', windowLoad);
+
+function windowLoad() {
+    if (document.querySelector('[data-glow]')) {
+        document.documentElement.addEventListener('mouseover', buttonActions);
+        document.documentElement.addEventListener('mouseout', buttonActions);
+        document.documentElement.addEventListener('mousemove', buttonActions);
+
+        let bGlow, bGlowColor, bGlowSize;
+
+        function buttonActions(e) {
+            const button = e.target.closest('[data-glow]');
+            if (!button) return;
+
+            if (e.type === "mouseover") {
+                button.insertAdjacentHTML("beforeend", `
+                <span class="button__glow">
+                    <span class="button__color"></span>
+                </span>
+                `);
+
+                bGlow = button.querySelector('.button__glow');
+                bGlowColor = button.querySelector('.button__color');
+
+                bGlowSize = Math.min(button.offsetWidth, button.offsetHeight);
+                bGlow.style.width = bGlow.style.height = `${bGlowSize}px`;
+
+                bGlowColor.style.width = `${button.offsetWidth}px`;
+                bGlowColor.style.height = `${button.offsetHeight}px`;
+            }
+
+            if (e.type === 'mouseout') {
+                button.querySelector('.button__glow').remove();
+            }
+
+            if (e.type === 'mousemove') {
+                const posX = e.pageX - (button.getBoundingClientRect().left + window.scrollX);
+                const posY = e.pageY - (button.getBoundingClientRect().top + window.scrollY);
+
+                bGlow.style.left = `${posX - bGlowSize / 2}px`;
+                bGlow.style.top = `${posY - bGlowSize / 2}px`;
+
+                bGlowColor.style.transform = `
+                translate(${posX - (button.offsetWidth - bGlowSize / 2)}px,
+                          ${posY - (button.offsetHeight - bGlowSize / 2)}px)`;
+            }
+        }
+    }
+}
+
+
+// Button animation
