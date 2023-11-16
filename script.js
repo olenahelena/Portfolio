d30f695f3379fcd32381142bd95d383c60e044b8
Olena Shevchenko <lemonslice67@gmail.com>
2023-11-16 18:57:22 +0100

    changed header

--- script.js
+++ script.js
@@ -54,4 +54,37 @@ function windowLoad() {
 }
 
 
-// Button animation
+// Header
+
+document.addEventListener("click", documentActions);
+
+function documentActions(e) {
+   const targetElement = e.target;
+
+   if (targetElement.closest('.icon-menu')) {
+       document.documentElement.classList.toggle('menu-open');
+    }
+
+   if (targetElement.closest('[data-goto]')) {
+    document.documentElement.classList.contains('menu-open') ?
+        document.documentElement.classList.remove('menu-open') : null;
+
+       const goTo = targetElement.closest('[data-goto]').dataset.goto;
+       const goToElement = document.querySelector(goTo);
+       const headerHeight = document.querySelector('.header').offsetHeight;
+
+       if (goToElement) {
+        window.scrollTo({
+            top: goToElement.offsetTop - (headerHeight + 15),
+            behavior: "smooth"
+        });
+       }
+       e.preventDefault();
+   }
+}
+
+// Download CV
+
+// Form
+
+// Footer
