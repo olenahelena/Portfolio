331c8ff05dda5994ff76dd81313c6885cd2cf143
Olena Shevchenko <lemonslice67@gmail.com>
2023-11-29 19:34:11 +0100

    changed form

--- script.js
+++ script.js
@@ -85,32 +85,3 @@ function documentActions(e) {
 // Download CV
 
 // Form
-
-const forms = document.forms;
-if (forms.length) {
-    for (const form of forms) {
-        form.addEventListener('submit', formSubmitAction);
-    }
-}
-async function formSubmitAction(e) {
-    e.preventDefault();
-    const form = e.target;
-    const formAction = form.getAttribute('action') ? form.getAttribute('action').trim() : "#";
-    const formMethod = form.getAttribute('method') ? form.getAttribute('method').trim() : "GET";
-    const formData = new FormData(form);
-
-    form.classList.add('form-sending');
-
-    const response = fetch(formAction, {
-        method: formMethod,
-        body: formData
-    })
-    if (response.ok) {
-        alert('Form sent!');
-        form.classList.remove('form-sending');
-        form.reset();
-    } else {
-        alert('Error');
-        form.classList.remove('form-sending');
-    }
-}
